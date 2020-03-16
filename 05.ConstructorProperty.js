5d2f63093c413388cd7cb095c3dff37c6b0a6303
Shohanur Rahman <shohan.drmc@gmail.com>
2020-03-16 23:56:23 +0600

    add constructorproperty.js

---
+++ 05.ConstructorProperty.js
@@ -0,0 +1,10 @@
+new String(); // '' , " ", ``
+new Boolean(); // true, false 
+new Number(); // 1, 2, 3
+
+/*  
+
+when we create object literal syntax 
+then Object is a built in constructor 
+
+*/
