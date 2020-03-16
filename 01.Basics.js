d811319876b2a516be5a6de467ef54e684e31249
Shohanur Rahman <shohan.drmc@gmail.com>
2020-03-16 21:41:21 +0600

    Basics.js added

---
+++ 01.Basics.js
@@ -0,0 +1,20 @@
+let radius = 1;
+let x = 1;
+let y = 1;
+
+// Object-Oriented Programming (OOP)
+
+const circle = {
+
+    radius: 1, // property
+    location: {
+        x: 1,
+        y: 1
+    },
+    isVisible: true,
+    draw: function () {
+        console.log('draw circle');
+    }
+};
+
+console.log(circle.draw()); // Method 
