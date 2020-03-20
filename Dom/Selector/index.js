71296d63710608b434aa1c5dadca48b7a05dc0f0
Shohanur Rahman <shohan.drmc@gmail.com>
2020-03-20 10:12:13 +0600

    adding simple dom append and delete

---
+++ Dom/Selector/index.js
@@ -0,0 +1,48 @@
+ document.getElementById("demo").innerHTML = "Hello World!";
+
+ let colors = ['red', 'green', 'yellow', 'blue'];
+ let btn = document.getElementById('clickMe')
+ let btn1 = document.querySelector('#clickMe1')
+ let i = 0
+
+ btn.addEventListener('click', function () {
+     //  alert('I am Click')
+     let h1 = document.getElementById("demo")
+     h1.innerHTML = "Text Change"
+
+     h1.style.background = colors[i]
+
+     count = i > colors.length ? i = 0 : i++
+     console.log(count);
+ })
+
+
+ // query selector
+ let container = document.querySelector('.container');
+
+ // change Style 
+ let j = document.querySelector('#demo')
+
+
+ let ssStyles = {
+     background: 'salmon',
+     fontSize: '30px',
+     fontFamily: 'Arial'
+ };
+
+ Object.assign(j.style, ssStyles);
+
+
+
+
+
+
+
+
+
+
+ // querySelector function
+
+ function $(selector) {
+     return document.querySelector(selector)
+ }
