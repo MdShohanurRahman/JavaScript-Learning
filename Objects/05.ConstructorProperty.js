74a6515567a245b9f1311938174cd2e1c20a0c03
Shohanur Rahman <shohan.drmc@gmail.com>
2020-03-25 08:35:18 +0600

    update constructorProperty.js

--- Objects/05.ConstructorProperty.js
+++ Objects/05.ConstructorProperty.js
@@ -1,3 +1,47 @@
+/* 
+javaScript has a property called constructor
+
+ */
+
+// factory function
+
+function createCircle(redius) {
+    return {
+        redius,
+        draw: function () {
+            console.log('draw');
+
+        }
+    };
+}
+
+const circle = createCircle(1)
+
+// constructor Function
+function Circle(redius) {
+    this.radius = radius;
+    this.draw = function () {
+        console.log('draw');
+
+    }
+}
+
+const another = new Circle(2);
+
+/*
+now see the console 
+-> another.constructor
+-> circle.constructor 
+ */
+
+/* 
+let x = {} is same as let x = new Object();
+*/
+
+
+
+// built in constructor
+
 new String(); // '' , " ", ``
 new Boolean(); // true, false 
 new Number(); // 1, 2, 3
